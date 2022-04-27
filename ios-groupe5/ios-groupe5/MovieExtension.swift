07261aab438e35c74953209878a2acb302ed051e
Ugo <ugo.prenat@gmail.com>
2022-04-27 14:26:59 +0200

    new Movie extension var

--- ios-groupe5/ios-groupe5/MovieExtension.swift
+++ ios-groupe5/ios-groupe5/MovieExtension.swift
@@ -13,6 +13,7 @@ struct Movie {
     let category: String
     let cover: String
     let url: String
+    let trailerUrl: String
     let duration: String
     let description: String
     let releaseDate: Int
@@ -26,6 +27,7 @@ extension Movie {
               let category = json["category"] as? String,
               let cover = json["cover"] as? String,
               let url = json["url"] as? String,
+              let trailerUrl = json["trailerUrl"] as? String,
               let duration = json["duration"] as? String,
               let description = json["description"] as? String,
               let releaseDate = json["releaseDate"] as? Int,
@@ -39,6 +41,7 @@ extension Movie {
         self.category = category
         self.cover = cover
         self.url = url
+        self.trailerUrl = trailerUrl
         self.duration = duration
         self.description = description
         self.releaseDate = releaseDate
