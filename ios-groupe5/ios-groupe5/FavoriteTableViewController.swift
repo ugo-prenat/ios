ba27a7c84ff6bdb82f6514be78d2af4330d2b247
Ugo <ugo.prenat@gmail.com>
2022-04-28 13:34:53 +0200

    Restart favorites page

--- ios-groupe5/ios-groupe5/FavoriteTableViewController.swift
+++ ios-groupe5/ios-groupe5/FavoriteTableViewController.swift
@@ -7,83 +7,99 @@
 
 import UIKit
 
-class FavoriteTableViewController: UITableViewController {
+class FavoriteTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let movie1: Movie = Movie(json: [
+          "title":"Vikings",
+          "category":"series",
+          "cover":"https://occ-0-6302-56.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYVZbqz_uitE0A1OmhKLo3WWuKlxVcPkV9Iaa2jtC-3OBvD1S8h27ov27K2A5e1Gvpt8TYTMPIroq_UtCn28Ho8rIxg.webp?r=eab",
+          "url":"https://file-examples.com/storage/fef12739526267ac9a2b543/2017/04/file_example_MP4_1920_18MG.mp4",
+          "trailerUrl":"https://youtu.be/mAl60ykBm4A",
+          "duration":"45min",
+          "description":"Cette série réaliste s'attache aux exploits du héros Ragnar Lothbrok qui ambitionne d'étendre le pouvoir viking à la faveur d'un chef manquant de vision politique.",
+          "releaseDate":2019,
+          "pegi":"16+"
+    ])!
+    let movie2: Movie = Movie(json: [
+          "title":"The Big Bang Theory",
+          "category":"series",
+          "cover":"https://occ-0-6302-56.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTe5Ok6xQUAl4vYOkTeSgJKMDlS5S3Jsbl-nWGobg6a1hblCYHSeqgQPluFtVizRr9nJQVExfVl2gzkK5IFMJKnKH7k.webp?r=bc4",
+          "url":"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+          "trailerUrl":"https://youtu.be/rCj-Fb1OmXg",
+          "duration":"32min",
+          "description":"Deux jeunes physiciens et leurs amis geeks voient leur cercle social s'agrandir lorsqu'une actrice en herbe emménage dans l'appartement à côté du leur.",
+          "releaseDate":2018,
+          "pegi":"13+"
+    ])!
+    let movie3: Movie = Movie(json: [
+          "title":"Inception",
+          "category":"movie",
+          "cover":"https://occ-0-6302-56.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABW9b6Ie6A9P-c3lBKZPd5XssEYzG69GTCxEevpVfkINEDdBgkbTQ1vUjcHKCTHvg9rYT3OJMBDvIO8dD1vnCxJIAVZ0.webp?r=bd6",
+          "url":"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+          "trailerUrl":"https://youtu.be/RCmNCkIUot8",
+          "duration":"2h28min",
+          "description":"no description",
+          "releaseDate":2010,
+          "pegi":"13+"
+    ])!
+    let movie4: Movie = Movie(json: [
+          "title":"The Walking Dead",
+          "category":"series",
+          "cover":"https://occ-0-6302-56.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABexI6hT4l9zeWIXJCeYzge9ah5SD6fcI5mZCzKEYn1L_CqcY2uBA6uugjxHhyGlwfptVbc_YkWUSOUrPvhOaeVkM-L8.webp?r=e82",
+          "url":"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+          "trailerUrl":"https://www.youtube.com/watch?v=AbtiqJGhWyY",
+          "duration":"67min",
+          "description":"Dans un monde apocalyptique sous l'emprise des zombies, des rescapés se rassemblent afin de lutter pour leur survie et de préserver leur humanité.",
+          "releaseDate":2019,
+          "pegi":"18+"
+    ])!
+    
+    
+    var favMovies: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+        
+        // Creating fav list
+        favMovies.append(movie1)
+        favMovies.append(movie2)
+        favMovies.append(movie3)
+        favMovies.append(movie4)
+        
     }
 
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.favMovies.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! FavoriteTableViewCell
 
-        // Configure the cell...
+        
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "moviePage") as? MoviePageViewController {
+            
+            vc.movie = favMovies[indexPath.row]
+            present(vc, animated: true, completion: nil)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
